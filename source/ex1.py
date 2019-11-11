6f862327c3f958504cae2b77d1d9d24e6c22bca7
DanielKovachev <danielkv8@gmail.com>
2019-11-12 00:00:36 +0200

    Add Ctor

--- source/ex1.py
+++ source/ex1.py
@@ -4,6 +4,14 @@ import math
 
 
 ids = ["111111111", "111111111"]
+passage_value = 10
+heroes_values = [11, 12, 13, 14]
+wall_value = 20
+pit_value = 30
+doors_values = [45, 46, 47, 48, 49]
+keys_values = [55, 56, 57, 58, 59]
+monster_value = 60
+gold_value = 70
 
 
 class WumpusProblem(search.Problem):
@@ -13,13 +21,29 @@ class WumpusProblem(search.Problem):
         """Don't forget to implement the goal test
         You should change the initial to your own representation.
         search.Problem.__init__(self, initial) creates the root node"""
-        initial = 0
+        #find goal
+        self.game_board = initial
+        self.heroes = []
+        self.doors = [(0,0), (0,0), (0,0), (0,0), (0,0)]
+        self.keys = [(0,0), (0,0), (0,0), (0,0), (0,0)]
+        for i in range(len(initial)):
+            for j in range(len(initial[0])):
+                if initial[i][j] in heroes_values:
+                    self.heroes.append((i,j))
+                elif initial[i][j] == gold_value:
+                    self.goal = gold_value
+                elif initial[i][j] in doors_values:
+                    self.doors[initial[i][j] - 45] = (i, j)
+                elif initial[i][j] in keys_values:
+                    self.keys[initial[i][j] - 55] = (i, j)
+        self.initial = 0
         search.Problem.__init__(self, initial)
         
     def actions(self, state):
         """Return the actions that can be executed in the given
         state. The result should be a tuple (or other iterable) of actions
         as defined in the problem description file"""
+        actions = []
 
     def result(self, state, action):
         """Return the state that results from executing the given
